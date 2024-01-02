780df9bd06c9404dae00b0828c13ec211cd6807f
bakhtiiartashbolotov <bakhtiiar.tashbolotov@alatoo.edu.kg>
2024-01-02 21:29:08 +0600

    Added a template for the main scene and scene changing functionality

--- src/main/java/com/bakhtiiar/todo/HelloController.java
+++ src/main/java/com/bakhtiiar/todo/HelloController.java
@@ -1,13 +1,34 @@
 package com.bakhtiiar.todo;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
-    private Label welcomeText;
+    public void onClickAddButton(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("addTaskWindow.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to Application!");
+    public void onClickConfirmButton(ActionEvent event) throws IOException{
+        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
