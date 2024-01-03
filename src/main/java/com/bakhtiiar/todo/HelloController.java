fa61d3fef3084dd45ecb5a38c55d6e99ffa5eaba
bakhtiiartashbolotov <bakhtiiar.tashbolotov@alatoo.edu.kg>
2024-01-03 16:29:42 +0600

    Added error alert when text fields and datePicker is empty

--- src/main/java/com/bakhtiiar/todo/HelloController.java
+++ src/main/java/com/bakhtiiar/todo/HelloController.java
@@ -6,6 +6,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.DatePicker;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -14,6 +15,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.Date;
+import java.util.Objects;
 
 public class HelloController {
 
@@ -37,12 +39,30 @@ public class HelloController {
     }
     @FXML
     public void onClickConfirmButton(ActionEvent event) throws IOException{
-        // Need to add error alert when text fields and datePicker is empty
+        if(titleInputField.getText().trim().isEmpty()){
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Title field is empty");
+            alert.setContentText("Write the title!");
+            alert.showAndWait();
+        } else if (descriptionInputField.getText().trim().isEmpty()) {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Description field is empty");
+            alert.setContentText("Write the description!");
+            alert.showAndWait();
+        } else if (datePicker.getValue() == null)
+        {
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setHeaderText("Date field is empty");
+            alert.setContentText("Select the date!");
+            alert.showAndWait();
+        }else{
+            Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        }
         // also task display functionality: when the user clicks the “Confirm” button, all data should be displayed in the ListView on the main page
-        Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
-        stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
+
     }
 }
